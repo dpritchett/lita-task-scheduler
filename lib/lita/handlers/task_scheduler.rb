d088e6698635671354fac793832528b4e7e57d63
Daniel J. Pritchett <dpritchett@gmail.com>
2018-04-07 13:35:40 -0500

    Nearly TDDed, need to refactor

--- lib/lita/handlers/task_scheduler.rb
+++ lib/lita/handlers/task_scheduler.rb
@@ -1,23 +1,87 @@
 require 'pry'
+require 'json'
 
 module Lita
   module Handlers
     class TaskScheduler < Handler
 
-      route /^schedule\s+"(.+)"\s+in\s+(.+)$/i, :schedule
-      route /^show schedule$/i, :show_schedule
+      route(/^schedule\s+"(.+)"\s+in\s+(.+)$/i, :create_schedule)
+      route(/^show schedule$/i, :show_schedule)
+
+      REDIS_TASKS_KEY = name.to_s
 
       def show_schedule(payload)
-        message = payload.matches.last
-        payload.reply "Scheduled tasks:"
+        schedule = redis.hgetall(REDIS_TASKS_KEY)
+
+        payload.reply schedule_report(schedule)
       end
 
-      def schedule(payload)
-        task,timing = payload.matches.last
+      def schedule_report(schedule)
+      end
+
+      def create_schedule(payload)
+        task, timing = payload.matches.last
         run_at = parse_timing(timing)
         puts run_at
         serialized = serialize_message(payload.message, new_body: task)
+
+        defer_task(serialized, run_at)
+
         resend(serialized)
+      end
+
+      def defer_task(serialized_task, run_at)
+        key_time = run_at.to_i.to_s
+
+        redis.watch(REDIS_TASKS_KEY)
+
+        tasks = redis.hget(REDIS_TASKS_KEY, key_time) || []
+
+        tasks = JSON.parse(tasks) unless tasks.empty?
+        tasks << serialized_task
+
+        redis.hset(REDIS_TASKS_KEY, key_time, tasks.to_json)
+
+        redis.unwatch
+
+        tasks
+      end
+
+      def execute_tasks(serialized_tasks)
+        serialized_tasks.each do |serialized_task|
+          Lita.logger.debug "Resending task #{serialized_task}"
+          resend serialized_task
+        end
+      end
+
+      def loop
+        loop do
+          tick
+          sleep 1
+        end
+      end
+
+      def tick
+        tasks = find_tasks_due
+        tasks.each { |t| resend t }
+        Lita.logger.debug "Task loop done for #{Time.now}"
+      end
+
+      def find_tasks_due
+        results = []
+        timestamps = redis.hkeys(REDIS_TASKS_KEY)
+
+        timestamps.each do |t|
+          key_time = Time.at(t.to_i)
+          next unless key_time <= Time.now
+
+          tasks_raw = redis.hget(REDIS_TASKS_KEY, t)
+          tasks = JSON.parse(tasks_raw)
+
+          results += tasks
+        end
+
+        results
       end
 
       def parse_timing(timing)
@@ -44,7 +108,7 @@ module Lita
       def rebroadcast(payload)
         serialized = serialize_message(payload.message)
 
-        key = "delay_#{rand(100..10000)}"
+        key = "delay_#{rand(100..10_000)}"
         redis.set(key, serialized.to_json)
         reloaded = JSON.parse redis.get(key), symbolize_names: true
 
--- spec/lita/handlers/task_scheduler_spec.rb
+++ spec/lita/handlers/task_scheduler_spec.rb
@@ -1,13 +1,13 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Lita::Handlers::TaskScheduler, lita_handler: true do
   let(:robot) { Lita::Robot.new(registry) }
 
   subject { described_class.new(robot) }
 
-  describe ':schedule' do
+  describe 'routing' do
     it { is_expected.to route('Lita schedule "show schedule" in 2 hours') }
-    it { is_expected.to route("Lita show schedule") }
+    it { is_expected.to route('Lita show schedule') }
 
     it 'Displays the schedule' do
       send_message 'Lita schedule "show schedule" in 1 second'
@@ -15,7 +15,73 @@ describe Lita::Handlers::TaskScheduler, lita_handler: true do
     end
   end
 
-  describe ':parse_timing_2' do
+  describe 'functionality' do
+    describe ':defer_task' do
+      it 'defers any single task' do
+        message = { canary_message: Time.now }
+        run_at = Time.now + 5
+        result = subject.defer_task(message, run_at)
+        expect(result).to include(message)
+      end
+
+      it 'stores multiple same-second tasks in an array' do
+        message = { 'canary_message' => Time.now.to_i }
+        run_at = Time.now + 5
+
+        5.times do
+          subject.defer_task(message, run_at)
+        end
+
+        result = subject.defer_task(message, run_at)
+
+        expect(result).to eq([message] * 6)
+      end
+    end
+
+    describe ':find_tasks_due' do
+      context 'two tasks are scheduled for five seconds ago' do
+        before { 2.times { subject.defer_task('past_task', Time.now - 5) } }
+
+        it 'returns all past due tasks' do
+          result = subject.find_tasks_due
+          expected = %w[past_task past_task]
+          expect(result).to eq(expected)
+        end
+      end
+
+      context 'one task scheduled in the future' do
+        before { subject.defer_task('future_task', Time.now + 100) }
+
+        it 'does not return that new task' do
+          result = subject.find_tasks_due
+          expect(result).to_not include('future_task')
+        end
+      end
+    end
+  end
+
+  describe 'execute_tasks' do
+    it 'resends each task' do
+      tasks = [{}, {}]
+
+      subject.stub(:resend)
+      expect(subject).to receive(:resend).exactly(2).times
+      subject.execute_tasks(tasks)
+    end
+  end
+
+  describe 'tick' do
+    before { subject.stub(:find_tasks_due).and_return ['a_task'] }
+
+    it 'should find tasks due and resend them' do
+      expect(subject).to receive(:find_tasks_due)
+      expect(subject).to receive(:resend).with('a_task')
+
+      subject.tick
+    end
+  end
+
+  describe ':parse_timing' do
     def time_drift(time, expected_seconds:)
       delta = (Time.now.utc - time).abs
       (expected_seconds - delta).abs
